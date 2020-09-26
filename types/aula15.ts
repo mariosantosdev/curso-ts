1d1f45b96454b160a71b89585285d3c151cb2a9f
nevr001 <playcar46@gmail.com>
2020-09-26 16:03:21 -0300

    Class 15

---
+++ types/aula15.ts
@@ -0,0 +1,9 @@
+/*
+    Variaveis opcionais com o Tipo Null
+*/
+
+let alturaOpicional: null | number
+alturaOpicional = 12
+console.log(alturaOpicional);
+alturaOpicional = null
+console.log(alturaOpicional);
