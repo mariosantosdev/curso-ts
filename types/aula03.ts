cd82d9e26e9ba54322a6a2c5517bb0dafe1f0210
nevr001 <playcar46@gmail.com>
2020-09-26 16:01:12 -0300

    Class 03

---
+++ types/aula03.ts
@@ -0,0 +1,9 @@
+/*
+    Aula 03 - Tipos explicitos
+*/
+
+let salario // OU `let salario: any`
+salario = 27.000
+console.log(typeof salario);
+salario = '27.000'
+console.log(typeof salario, '\n');
