b289a3b7e2c7d6376e2c0eabaa477353e53dcc06
nevr001 <playcar46@gmail.com>
2020-09-28 17:35:44 -0300

    Challenge 06

---
+++ desafios/desafio06.ts
@@ -0,0 +1,10 @@
+/*
+    Transformar codigo js da video aula em codigo ts
+
+    Adicionar todos os elementos do exercicio anterior em uma nova array
+*/
+
+let nums2: number[] = [-3, 33, 38, 5]
+let array: number[] = [...nums2]
+
+console.log(array)
