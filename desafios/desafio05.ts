1d49396a3fce8c5d765cd065d00b1f8c1691b0b7
nevr001 <playcar46@gmail.com>
2020-09-28 17:35:40 -0300

    Challenge 05

---
+++ desafios/desafio05.ts
@@ -0,0 +1,9 @@
+/*
+    Transformar codigo js da video aula em codigo ts
+
+    var nums = [-3, 33, 38, 5]
+    console.log('???') // Deve exibir o menor valor
+*/
+
+let nums: number[] = [-3, 33, 38, 5]
+console.log(Math.min(...nums))
