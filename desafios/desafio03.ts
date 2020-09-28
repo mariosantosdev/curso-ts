55ad2acdd56acaf353c4b596832cb0c0c3b5d53e
nevr001 <playcar46@gmail.com>
2020-09-28 17:35:32 -0300

    Challenge 03

---
+++ desafios/desafio03.ts
@@ -0,0 +1,13 @@
+/*
+    Transformar codigo js da video aula em codigo ts
+
+    var dobro = function(valor){
+        return valor * 2
+    }
+    console.log(dobro(10))
+*/
+
+let Dobro = (valor: number): number => {
+    return valor * 2
+}
+console.log(Dobro(10))
