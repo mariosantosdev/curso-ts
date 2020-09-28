e4294b5214e0265c0352e173b7f88a51c6672e22
nevr001 <playcar46@gmail.com>
2020-09-28 17:35:48 -0300

    Challenge 07

---
+++ desafios/desafio07.ts
@@ -0,0 +1,13 @@
+/*
+    Transformar codigo js da video aula em codigo ts
+
+    var notas = [8.5, 6.3, 9.4]
+    var nota1 = notas[1]
+    var nota2 = notas[2]
+    var nota3 = notas[3]
+    console.log(nota1, nota2, nota3)
+*/
+
+let notas: number[] = [8.5, 6.3, 9.4]
+let [nota1, nota2, nota3] = notas
+console.log(nota1, nota2, nota3)
