5a8a6656d92dd58c9f95edb0ed1edb12090e6b00
nevr001 <playcar46@gmail.com>
2020-10-10 10:08:03 -0300

    Namespace Class 01

---
+++ namespaces/aula01.ts
@@ -0,0 +1,17 @@
+/*
+    Namespace são espaços reservados para os códigos não conflitarem com escopo global
+*/
+
+namespace Areas {
+    const PI = 3.14
+
+    export function circunferencia(raio: number): number {
+        return PI * Math.pow(raio, 2)
+    }
+
+    export function retangulo(base: number, altura: number): number {
+        return base * altura
+    }
+}
+console.log(Areas.circunferencia(10))
+console.log(Areas.retangulo(20, 10))
